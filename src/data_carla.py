1a4e72cffdef4aa03e6c16e98e223defdc6c4085
yiyang_computation <ourxxbluesky@gmail.com>
2021-02-26 10:39:47 -0800

    added carla data structure for loader

---
+++ src/data_carla.py
@@ -0,0 +1,81 @@
+"""
+Copyright (C) 2021 Yiyang Zhou.  All rights reserved.
+Authors: Yiyang Zhou
+"""
+
+import torch
+import os
+import numpy as np
+from PIL import Image
+import cv2
+from pyquaternion import Quaternion
+from nuscenes.nuscenes import NuScenes
+from nuscenes.utils.splits import create_splits_scenes
+from nuscenes.utils.data_classes import Box
+from glob import glob
+
+from .tools import get_lidar_data, img_transform, normalize_img, gen_dx_bx
+
+class CarlaData(torch.utils.data.Dataset):
+    def __init__(self,nusc,is_trrain,data_aug_conf, grid_conf):
+        print("EMPTY")
+
+
+
+class VizData(NuscData):
+    def __init__(self, *args, **kwargs):
+        super(VizData, self).__init__(*args, **kwargs)
+    
+    def __getitem__(self, index):
+        rec = self.ixes[index]
+        
+        cams = self.choose_cams()
+        imgs, rots, trans, intrins, post_rots, post_trans = self.get_image_data(rec, cams)
+        lidar_data = self.get_lidar_data(rec, nsweeps=3)
+        binimg = self.get_binimg(rec)
+        
+        return imgs, rots, trans, intrins, post_rots, post_trans, lidar_data, binimg
+
+
+class SegmentationData(NuscData):
+    def __init__(self, *args, **kwargs):
+        super(SegmentationData, self).__init__(*args, **kwargs)
+    
+    def __getitem__(self, index):
+        rec = self.ixes[index]
+
+        cams = self.choose_cams()
+        imgs, rots, trans, intrins, post_rots, post_trans = self.get_image_data(rec, cams)
+        binimg = self.get_binimg(rec)
+        
+        return imgs, rots, trans, intrins, post_rots, post_trans, binimg
+
+
+def worker_rnd_init(x):
+    np.random.seed(13 + x)
+
+    
+def compile_data(version, dataroot, data_aug_conf, grid_conf, bsz,
+                 nworkers, parser_name):
+    nusc = NuScenes(version='v1.0-{}'.format(version),
+                    dataroot=os.path.join(dataroot, version),
+                    verbose=False)
+    parser = {
+        'vizdata': VizData,
+        'segmentationdata': SegmentationData,
+    }[parser_name]
+    traindata = parser(nusc, is_train=True, data_aug_conf=data_aug_conf,
+                         grid_conf=grid_conf)
+    valdata = parser(nusc, is_train=False, data_aug_conf=data_aug_conf,
+                       grid_conf=grid_conf)
+
+    trainloader = torch.utils.data.DataLoader(traindata, batch_size=bsz,
+                                              shuffle=True,
+                                              num_workers=nworkers,
+                                              drop_last=True,
+                                              worker_init_fn=worker_rnd_init)
+    valloader = torch.utils.data.DataLoader(valdata, batch_size=bsz,
+                                            shuffle=False,
+                                            num_workers=nworkers)
+
+    return trainloader, valloader
