78c82397b42138b8b9efe4516f4bc2195c29b46b
yiyang_computation <ourxxbluesky@gmail.com>
2021-04-23 14:41:50 -0700

    Added FastRCNN and direct parameter prediction

---
+++ src/fast_RCNN/rcnn_train.py
@@ -0,0 +1,246 @@
+from detection.engine import train_one_epoch, evaluate
+import detection.utils
+
+import os
+import numpy as np
+import torch
+from PIL import Image
+
+
+class PennFudanDataset(object):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+
+    def __getitem__(self, idx):
+        # load images and masks
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+        img = Image.open(img_path).convert("RGB")
+        # note that we haven't converted the mask to RGB,
+        # because each color corresponds to a different instance
+        # with 0 being background
+        mask = Image.open(mask_path)
+        # convert the PIL Image into a numpy array
+        mask = np.array(mask)
+        # instances are encoded as different colors
+        obj_ids = np.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+class CarlaDataset(object):
+    def __init__(self, root, transforms):
+        self.root = root
+        self.transforms = transforms
+        self.is_train=True
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.ixes= self.prepro()
+        self.imgs = list(sorted(os.listdir(os.path.join(root, "PNGImages"))))
+        self.masks = list(sorted(os.listdir(os.path.join(root, "PedMasks"))))
+
+    def prepro(self):
+        """
+        Return a list of all scenes, each scene is a dictionary with 'label' and 'inputs' for 6 cameras
+        Incomplete data are elimited here.  
+        DONE: scenes are incomplete on Mar.08, from Takeda in email. YZ. 
+        """
+        data_summary=[]
+        if self.is_train:
+            start_folder=0
+            end_folder=69
+        else:
+            start_folder=70
+            end_folder=99
+
+        input_folder= sorted(glob(os.path.join(self.root, "preprocessed",'bev_rgb_acc_inpaint-*'))):
+            for input_image_dir in input_folder:
+                label_folder_index= input_image_dir[20:26]
+                label_file_index= inputimage_dir_[27:33]
+                data_summary.append({'label': os.path.join(self.root,label_folder_index,"bev_keypoint_{},png".format(label_file_index) ), 'input_dir':input_image_dir})
+        return data_summary
+
+    def get_keypoints(self,rec):
+        keypts_path= rec['label']
+        green_value= (0,255,255)
+        red_value= (255,0,255)
+
+        image=Image.open(keypts_path)
+        raw_pixel= np.asarray(image)
+        green_pixels= np.vstack(np.where(np.all(raw_pixel==green_value,axis=-1)==True)) 
+        g_pts= int(green_pixels.shape[1])/65
+        red_pixels= np.vstacknp.where(np.all(raw_pixel==red_value,axis=-1)==True) 
+        r_pts= int(red_pixels.shape[1])/65
+
+        g_fit=KMeans(n_clusters=g_pts, random_state=0).fit(green_pixels.T) 
+        r_fit=KMeans(n_clusters=r_pts, random_state=0).fit(red_pixels.T)
+
+        return g_fit.cluster_centers_, r_fit.cluster_centers_
+
+
+    def __getitem__(self, idx):
+        rec = self.ixes[idx]
+        
+        cams = self.choose_cams()
+        orig_img, imgs, rots, trans, intrins, post_rots, post_trans = self.get_image_data(rec, cams)
+        g_centers, r_centers = self.get_binimg(rec)
+
+        img= Image.open(self.ixes[idx]['input_dir']).convert("RGB")
+        target= self.get_keypoints()
+    
+        # load images and masks
+        img_path = os.path.join(self.root, "PNGImages", self.imgs[idx])
+        mask_path = os.path.join(self.root, "PedMasks", self.masks[idx])
+        img = Image.open(img_path).convert("RGB")
+        # note that we haven't converted the mask to RGB,
+        # because each color corresponds to a different instance
+        # with 0 being background
+        mask = Image.open(mask_path)
+        # convert the PIL Image into a numpy array
+        mask = np.array(mask)
+        # instances are encoded as different colors
+        obj_ids = np.unique(mask)
+        # first id is the background, so remove it
+        obj_ids = obj_ids[1:]
+
+        # split the color-encoded mask into a set
+        # of binary masks
+        masks = mask == obj_ids[:, None, None]
+
+        # get bounding box coordinates for each mask
+        num_objs = len(obj_ids)
+        boxes = []
+        for i in range(num_objs):
+            pos = np.where(masks[i])
+            xmin = np.min(pos[1])
+            xmax = np.max(pos[1])
+            ymin = np.min(pos[0])
+            ymax = np.max(pos[0])
+            boxes.append([xmin, ymin, xmax, ymax])
+
+        # convert everything into a torch.Tensor
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+        # there is only one class
+        labels = torch.ones((num_objs,), dtype=torch.int64)
+        masks = torch.as_tensor(masks, dtype=torch.uint8)
+
+        image_id = torch.tensor([idx])
+        area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        # suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {}
+        target["boxes"] = boxes
+        target["labels"] = labels
+        target["masks"] = masks
+        target["image_id"] = image_id
+        target["area"] = area
+        target["iscrowd"] = iscrowd
+
+        if self.transforms is not None:
+            img, target = self.transforms(img, target)
+
+        return img, target
+
+    def __len__(self):
+        return len(self.imgs)
+
+
+def main():
+    # train on the GPU or on the CPU, if a GPU is not available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # our dataset has two classes only - background and person
+    num_classes = 2
+    # use our dataset and defined transformations
+    dataset = PennFudanDataset('PennFudanPed', get_transform(train=True))
+    dataset_test = PennFudanDataset('PennFudanPed', get_transform(train=False))
+
+    # split the dataset in train and test set
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=4,
+        collate_fn=utils.collate_fn)
+
+    # get the model using our helper function
+    model = get_model_instance_segmentation(num_classes)
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    # let's train it for 10 epochs
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
+
+    print("That's it!")
---
+++ src/image_regression/train_image_reg.py
@@ -0,0 +1,242 @@
+from sklearn.cluster import KMeans
+import numpy as np 
+import torch 
+import torchvision
+from PIL import Image
+import math 
+from torchvision import transforms
+from glob import glob
+from tensorboardX import SummaryWriter
+import os 
+
+class CarlaData(object):
+    def __init__(self, root, is_train):
+        self.root = root
+        self.is_train=is_train
+        # load all image files, sorting them to
+        # ensure that they are aligned
+        self.ixes= self.prepro()
+
+    def prepro(self):
+        """
+        Return a list of all scenes, each scene is a dictionary with 'label' and 'inputs' for 6 cameras
+        Incomplete data are elimited here.  
+        DONE: scenes are incomplete on Mar.08, from Takeda in email. YZ. 
+        """
+        data_summary=[]
+        if self.is_train:
+            start_folder=0
+            end_folder=69
+        else:
+            start_folder=70
+            end_folder=99
+
+        input_folder= sorted(glob(os.path.join(self.root, "preprocessed",'bev_rgb_acc_inpaint-*')))
+        for input_image_dir in input_folder:
+            label_folder_index= input_image_dir[-17:-11]
+            label_file_index= input_image_dir[-10:-4]
+            data_summary.append({'label': os.path.join(self.root,label_folder_index,"bev_keypoint_{}.png".format(label_file_index) ), 'input_dir':input_image_dir})
+        return data_summary
+
+    def get_keypoints(self,rec):
+        keypts_path= rec['label']
+        green_value= (0,255,255)
+        red_value= (255,0,255)
+
+
+        image=Image.open(keypts_path)
+        raw_pixel= np.asarray(image)
+        green_pixels= np.vstack(np.where(np.all(raw_pixel==green_value,axis=-1)==True)) 
+        red_pixels= np.vstack(np.where(np.all(raw_pixel==red_value,axis=-1)==True))
+        g_pts_count= int(green_pixels.shape[1]/65)
+        r_pts_count= int(red_pixels.shape[1]/65)
+
+        if g_pts_count==0 or r_pts_count==0:
+            return np.zeros([14])
+
+        g_fit=KMeans(n_clusters=g_pts_count, random_state=0).fit(green_pixels.T) 
+        r_fit=KMeans(n_clusters=r_pts_count, random_state=0).fit(red_pixels.T)
+
+        g_pts= g_fit.cluster_centers_
+        r_pts= r_fit.cluster_centers_
+        all_pts= np.vstack((g_pts,r_pts))
+        
+        #center:
+        center= np.mean(all_pts,axis=0).reshape(2,1)
+        #up:
+        up_pts= all_pts[np.where( (all_pts[:,0]<all_pts[:,1]) * (all_pts[:,0]<1000- all_pts[:,1]))]
+        if up_pts.shape[0]==0:
+            up_num=np.array([[0]])
+            up_center= np.array([[0],[0]])
+        else:
+            up_num= up_pts.shape[0]
+            up_center= np.mean(up_pts,axis=0,dtype=int).reshape(2,1)
+
+        #bottom:
+        bottom_pts= all_pts[np.where( (all_pts[:,0]>all_pts[:,1]) * (all_pts[:,0]>1000- all_pts[:,1]))]
+        if bottom_pts.shape[0]==0:
+            bottom_num=np.array([[0]])
+            bottom_center= np.array([[0],[0]])
+        else:
+            bottom_num= bottom_pts.shape[0]
+            bottom_center= np.mean(bottom_pts,axis=0,dtype=int).reshape(2,1)
+
+        #left:
+        left_pts= all_pts[np.where( (all_pts[:,0]>all_pts[:,1]) * (all_pts[:,0]<1000- all_pts[:,1]))]
+        if left_pts.shape[0]==0:
+            left_num=np.array([[0]])
+            left_center= np.array([[0],[0]])
+        else:
+            left_num= left_pts.shape[0]
+            left_center= np.mean(left_pts,axis=0,dtype=int).reshape(2,1)
+
+        #right:
+        right_pts= all_pts[np.where( (all_pts[:,0]<all_pts[:,1]) * (all_pts[:,0]>1000- all_pts[:,1]))]
+        if right_pts.shape[0]==0:
+            right_num=np.array([[0]])
+            right_center= np.array([[0],[0]])
+        else:
+            right_num= right_pts.shape[0]
+            right_center= np.mean(right_pts,axis=0,dtype=int).reshape(2,1)
+
+        #Returning a 14 dimensional vector 
+        return np.vstack((center, up_num, up_center, bottom_num, bottom_center, left_num, left_center, right_num, right_center )).reshape(14,)
+
+    def get_image_data(self, rec):
+        """
+        Return the image normalized by tools.normalize
+        """
+        input_image = Image.open(rec["input_dir"])
+        preprocess = transforms.Compose([
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        input_tensor = preprocess(input_image)
+
+        return input_tensor
+
+    def __getitem__(self, idx):
+        rec = self.ixes[idx]
+        input_tensor = self.get_image_data(rec)
+        target_tensor= torch.Tensor(self.get_keypoints(rec))
+        #if self.transforms is not None:
+        #    img, target = self.transforms(img, target)
+
+        return input_tensor, target_tensor
+
+    def __len__(self):
+        return len(self.ixes)
+
+
+def compile_data(dataroot, bsz, nworkers):
+
+    traindata= CarlaData(root= dataroot, is_train= True)
+    valdata= CarlaData(root= dataroot, is_train=False)
+    
+    trainloader = torch.utils.data.DataLoader(traindata, batch_size=bsz,
+                                            shuffle=True,
+                                            num_workers=nworkers,
+                                            drop_last=True)
+    valloader = torch.utils.data.DataLoader(valdata, batch_size=bsz,
+                                            shuffle=False,
+                                            num_workers=nworkers)
+    return trainloader, valloader 
+"""
+def tensorboard_visualiza(model, writer, dataloader, is_train, device):
+    sampeld_image_data= iter(dataloader)
+    img, target= sampeld_image_data.next()
+    #print(rec['label'])
+    # Display the input image set 
+    
+    first_image_set= torch.unbind(img[0])
+    first_label= torch.unbind(target[0])
+
+    raw_img= first_image_set
+    
+
+
+
+
+
+
+
+    img_grid= torchvision.utils.make_grid(tensor= first_image_set, nrow=1)
+    if is_train:
+        writer.add_image('train_input', img_grid)
+    else: 
+        writer.add_image('val_input',img_grid)
+
+    # Display the GT label:
+    first_label= list(torch.unbind(binimgs_display[0]))
+    img_grid_gt= torchvision.utils.make_grid(first_label)
+    if is_train:
+        writer.add_image('train_gt', img_grid_gt)
+    else: 
+        writer.add_image('val_gt',img_grid_gt)
+
+    # Display the prediction label 
+    preds = model(imgs.to(device), rots.to(device),
+                          trans.to(device), intrins.to(device), post_rots.to(device),
+                          post_trans.to(device))
+    preds = (preds > 0)
+    preds = preds*255
+    first_pred_set= list(torch.unbind(preds[0]))
+    img_grid_pred= torchvision.utils.make_grid(first_pred_set)
+    if is_train:
+        writer.add_image('train_pred', img_grid_pred)
+    else: 
+        writer.add_image('val_pred',img_grid_pred)
+"""
+
+
+if __name__ == '__main__':
+
+     # The data preparation
+     data_root= "/media/m/377445e5-f724-4791-861c-730d2b0bba3f/carla_map_bev_10k_v2"
+     logdir= "/home/m/lift-splat-shoot/src/image_regression/log"
+
+     bsz=6
+     nworkers=10            
+     lr=1e-3
+     weight_decay=1e-7
+     gpuid=0
+     num_epochs=5
+     counter = 0
+
+     trainloader, valloader = compile_data(data_root, bsz=bsz, nworkers=nworkers)
+    
+     device = torch.device('cpu') if gpuid < 0 else torch.device(f'cuda:{gpuid}')
+
+     model= torchvision.models.resnet18(pretrained=True)
+     model.fc= torch.nn.Linear(512,14)
+     model.to(device)
+     opt = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
+     loss_fn = torch.nn.MSELoss()
+
+     writer = SummaryWriter(logdir=logdir)
+
+
+     model.train()
+     for epoch in range(num_epochs):
+         for batch_idx, (data, target) in enumerate(trainloader):
+             data, target = data.to(device), target.to(device)
+             opt.zero_grad()
+             output = model(data)
+             loss = loss_fn(output, target)
+             loss.backward()
+             opt.step()
+             counter += 1
+
+             if counter % 10 == 0:
+                 print(counter, loss.item())
+                 writer.add_scalar('train/loss', loss, counter)
+
+             #if counter % 50 == 0:
+                #tensorboard_visualiza(model= model, writer= writer, dataloader= trainloader, is_train=1, device= device)
+
+
+     print("That's it!")
+
+
